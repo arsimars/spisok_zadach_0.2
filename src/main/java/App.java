e1dcfa23554aab5333c09e4657adb82f65c4e4b9
arsimars <marsiars@mail.ru>
2024-05-24 13:11:23 +0300

    добавил logback (наверное) но появляется какая то красная надпись при сработывание его

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,17 +1,31 @@
+
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Main {
+public class App {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
+        System.setProperty("LOG_LEVEL", "info");
+
+        logger.trace("Entering method foo()");
+        logger.debug("Received request from 198.12.34.56");
+        logger.info("User logged in: john");
+        logger.warn("Connection to server lost. Retrying...");
+        logger.error("Failed to write data to file: myFile.txt");
         Scanner sc = new Scanner(System.in);
         String zadacha = "";
         String vipolni = "[]";
         Integer nomerchek = 0;
         String nechego = "";
-        ArrayList<Zadanie> spisok = new ArrayList ();
-        ArrayList<Command> commands = new ArrayList ();
-
+        ArrayList<Zadanie> spisok = new ArrayList();
+        ArrayList<Command> commands = new ArrayList();
         commands.add(new Command(" 1.Формат команды: add <описание задачи>\n" + "\n" + "Описание задачи может содержать любые символы, кроме перевода строки.\n" + "Перевод строки (нажатие клавиши Enter) означает завершение ввода описания задачи.\n"));
         commands.add(new Command("2.Формат команды: print [all]\n" + "\n" + "Выводит на печать список задач.\n"));
         commands.add(new Command("3.формат команды: print\n" + "По-умолчанию выводятся только невыполненные задачи\n"));
@@ -21,6 +35,9 @@ public class Main {
         commands.add(new Command("7.Формат команды: delete <идентификатор задачи>\n" + "\n" + "Удаляет задачу из списка задач.\n"));
         commands.add(new Command("8.Формат команды: edit <идентификатор задачи> <новое значение>\n" + "\n" + "Меняет описание задачи.\n"));
         while (sc.hasNext()) {
+            Logger logger = LoggerFactory.getLogger(App.class);
+            logger.info("ДЖАВА ЭТО МОЩЬ!");
+
             String command = sc.next();
             if (command.equals("add")) {
                 zadacha = sc.nextLine();
@@ -37,19 +54,18 @@ public class Main {
             } else if (command.equals("quit")) {
                 System.exit(0);
             } else if (command.equals("print")) {
-                String K = sc.nextLine();
-                if (K.equals("all")) {
+                String allnet = sc.nextLine();
+                if (allnet.contains("all")) {
                     for (Zadanie f : spisok) {
                         System.out.println(f);
                     }
-                } else if (K.equals(nechego)) {
+                } else if (allnet.equals(nechego)) {
                     for (Zadanie t : spisok) {
                         if (t.vipolni.equals("[]")) {
                             System.out.println(t);
                         }
                     }
-                } else ;
-                {
+                } else {
                     System.out.println("не коретно введена команда");
                 }
             } else if (command.equals("toggle")) {
@@ -115,6 +131,7 @@ public class Main {
                 }
             }
         }
-    }
 
+    }
 }
+
